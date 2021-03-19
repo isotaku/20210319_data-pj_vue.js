4107645a7d87a927bae2692ec701380ac8060e3a
isotaku <takuiso0214@icloud.com>
2021-03-19 23:20:09 +0900

    first-commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  async created() {
+    await this.$store.dispatch("changeDataAction1");
+    await this.$store.dispatch("changeDataAction2");
+    console.log(this.$store.state.data1.data);
+    console.log(this.$store.state.data2.data);
+  },
+};
 </script>
